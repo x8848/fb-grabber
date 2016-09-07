e8c4bdb8f4917baec038d6cc8c462111e84ebb21
dmytroz <dmytro.zhuravlov@playtech.com>
2016-09-07 18:25:17 +0300

    add socket.io

--- app.js
+++ app.js
@@ -1,55 +1,76 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('group', function (name) {
+        console.log('group: ' + name);
+        var time = Date.now();
+        var url = FACEBOOK + "groups/" + name + "/members/";
+        var browser = getBrowser();
+        login(browser)
+            .then(browser.get(url))
+            .then(() => isMoreButton(browser))
+            .then(p => loadAllPeople(p, browser))
+            .then(findPeople)
+            .then(ids => sendResponse(name, ids, time));
+        browser.quit();
+    });
+    socket.on('page', function (name) {
+        console.log('page: ' + name);
+        var time = Date.now();
+        var url = FACEBOOK + name + "/";
+        var browser = getBrowser();
+        login(browser)
+            .then(browser.get(url))
+            .then(() => browser.findElement(By.css("meta[property='al:android:url']")))
+            .then(e => e.getAttribute("content"))
+            .then(s => browser.get(FACEBOOK + "/search/" + (s.split("/")[3]) + "/likers/"))
+            .then(() => isAllPeople(browser))
+            .then(p => scrollPeople(p, browser))
+            .then(parsePeople)
+            .then(ids => sendResponse(name, ids, time));
+        browser.quit();
+    });
+});
+
 var webdriver = require('selenium-webdriver');
 var chrome = require('selenium-webdriver/chrome');
 var By = webdriver.By;
-var browser = new webdriver.Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(new chrome.Options().addArguments("--incognito"))
-    .build();
 
 var FACEBOOK = "https://www.facebook.com/";
-var MORE_BUTTON = "clearfix pam uiMorePager stat_elem morePager _52jv";
+var MORE_BUTTON = "pam uiBoxLightblue uiMorePagerPrimary";
 
-app.get('/page/:id', function (req, res) {
-    url = FACEBOOK + req.params.id + "/";
-    browser.get(url)
-        .then(()=> browser.findElement(By.css("meta[property='al:android:url']")))
-        .then(e => e.getAttribute("content"))
-        .then(s => browser.get(FACEBOOK + "/search/" + (s.split("/")[3]) + "/likers/"))
-        .then(checkAllPeople)
-        .then(scrollAllPeople)
-        .then(parsePeople)
-        .then(res.send.bind(res));
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
 });
 
-app.get('/group/:id', function (req, res) {
-    url = FACEBOOK + "groups/" + req.params.id + "/members/";
-    browser.get(url)
-        .then(browser.isElementPresent(By.className(MORE_BUTTON)))
-        .then(loadAllPeople)
-        .then(findPeople)
-        .then(res.send.bind(res));
-});
+function getBrowser() {
+    return new webdriver.Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(new chrome.Options().addArguments("--incognito"))
+        .build();
+}
 
-app.listen(3000, function () {
-    console.log('app listening on port 3000');
-});
+function login(browser) {
+    return browser.get(FACEBOOK)
+        .then(() => browser.findElement(By.id('email')).sendKeys('EnverNosov@bk.ru'))
+        .then(() => browser.findElement(By.id('pass')).sendKeys('gJVFfj5B'))
+        .then(() => browser.findElement(By.id('loginbutton')).click());
+}
 
-browser.get(FACEBOOK);
-browser.findElement(By.id('email')).sendKeys('EnverNosov@bk.ru');
-browser.findElement(By.id('pass')).sendKeys('gJVFfj5B');
-browser.findElement(By.id('loginbutton')).click();
-
-function checkAllPeople() {
+function isAllPeople(browser) {
     return browser.isElementPresent(By.id("browse_end_of_results_footer"));
 }
 
-function scrollAllPeople(present) {
+function scrollPeople(present, browser) {
     if (!present) {
         return browser.executeScript("window.scrollTo(0, document.body.scrollHeight)")
-            .then(checkAllPeople)
-            .then(scrollAllPeople);
+            .then(() => isAllPeople(browser))
+            .then(p => scrollPeople(p, browser));
     } else {
         return browser.sleep(2000).then(()=> browser.findElements(By.className("_3u1 _gli _5und")))
     }
@@ -63,11 +84,19 @@ var parsePeople = function (people) {
     });
 };
 
-var loadAllPeople = function (present) {
+function isMoreButton(browser) {
+    return browser.isElementPresent(By.className(MORE_BUTTON));
+}
+
+var loadAllPeople = function (present, browser) {
     if (present) {
-        return browser.findElement(By.className(MORE_BUTTON)).click().then(loadAllPeople);
+        var button = browser.findElement(By.className(MORE_BUTTON));
+        return browser.actions().mouseMove(button).click().perform()
+            .then(browser.sleep(2000))
+            .then(() => isMoreButton(browser))
+            .then(p => loadAllPeople(p, browser));
     } else {
-        return browser.sleep(2000).then(()=>browser.findElements(By.css("div.fsl.fwb.fcb a")));
+        return browser.sleep(2000).then(() => browser.findElements(By.css("div.fsl.fwb.fcb a")));
     }
 };
 
@@ -78,3 +107,9 @@ var findPeople = function (people) {
         return people.map(p => p.split("?id=")[1].split("&")[0]);
     });
 };
+
+function sendResponse(name, ids, time) {
+    var date = new Date(Date.now() - time);
+    time = date.getUTCMinutes() + ':' + date.getUTCSeconds();
+    io.emit('groupResult', {name: name, total: ids.length, time: time, ids: ids});
+}
