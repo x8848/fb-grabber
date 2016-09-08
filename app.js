f3bb7eba3e397f4cc3f3518a184554200ba7c223
dmytroz <dmytro.zhuravlov@playtech.com>
2016-09-08 11:56:55 +0300

    change app listen port to default 80

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(3000);
+server.listen(80);
 
 io.on('connection', function (socket) {
     console.log('a user connected');
